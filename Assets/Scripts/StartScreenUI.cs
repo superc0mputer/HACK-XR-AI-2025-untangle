126121bea95a895e499fa2b91e234d2b8dadfe7a
Vulture2001 <73824892+Vulture2001@users.noreply.github.com>
2025-11-13 21:16:19 +0100

    Add background asset and font

--- Assets/Scripts/StartScreenUI.cs
+++ Assets/Scripts/StartScreenUI.cs
@@ -7,6 +7,9 @@ public class StartScreen : MonoBehaviour
 {
     [Header("UI Elements")]
     public TextMeshProUGUI welcomeText;
+    public TextMeshProUGUI clockText;
+    public TextMeshProUGUI dateText;
+    public TextMeshProUGUI phrase;
     public Button startButton;
     public string username = "Alex";
     
@@ -28,10 +31,10 @@ public class StartScreen : MonoBehaviour
         string date = now.ToString("dddd, MMMM dd");
         string time = now.ToString("h:mm tt");
 
-        welcomeText.text = $"{greeting}, {username}\n\n" +
-                           $"Today is {date}\n" +
-                           $"{time}\n\n" +
-                           "Are you ready to get started?";
+        clockText.text = $"{time}\n\n";
+        dateText.text = $"Today is {date}\n +";
+        welcomeText.text = $"{greeting}, {username}\n\n";
+        phrase.text = $"Are you ready to get started?";
     }
 
     void OnStartButtonPressed()
